6b1301f66c8f5659ea8ac263c4d469c5024644d7
Bhuwan297 <66291102+Bhuwan297@users.noreply.github.com>
2025-09-25 22:02:12 +1000

    Copying the files
    Copying the files

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,9 @@
+// Assets/Scripts/SceneLoader.cs
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadBhuwanScene()  => SceneManager.LoadScene("BhuwanScene");
+    public void LoadVarshitScene() => SceneManager.LoadScene("VarshitScene");
+}
